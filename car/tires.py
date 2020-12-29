58af0240ecb0ad9921bf9425fe7f0838131a2040
Cameron Winter <10819480+XtremeCookiez@users.noreply.github.com>
2020-12-29 16:09:53 -0600

    Started tires

---
+++ car/tires.py
@@ -0,0 +1,7 @@
+class Tire:
+    def __init__(self, static, static_lateral, kinetic, kinetic_lateral):
+        self.static = static
+        self.static_lateral = static_lateral
+        self.kinetic = kinetic
+        self.kinetic_lateral = kinetic_lateral
+    
